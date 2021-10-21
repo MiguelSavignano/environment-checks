0cec88e41b462c1925a92170757bf6d907de0c6a
Miguel Savignano <migue.masx@gmail.com>
2021-10-21 14:24:50 +0200

    test

--- src/EnvironmentChecks.js
+++ src/EnvironmentChecks.js
@@ -7,39 +7,19 @@ class EnvironmentChecks extends EnvironmentChecksBase {
     const defaultMessages = {
       dotenv: ['', `You need to create the .env file with the correct values`],
       npm_auth: ['', `You need to login with npm`],
-      gcloud_config: [
-        '',
-        `You need to login with gcloud (gcloud auth application-default login)`,
-      ],
+      gcloud_config: ['', `You need to login with gcloud (gcloud auth application-default login)`],
       docker: ['Docker :whale:', '(https://docs.docker.com/get-docker/)'],
-      docker_compose: [
-        'Docker compose :whale: :whale:',
-        '(https://docs.docker.com/compose/install/)',
-      ],
-      nginx: [
-        'Ready for start nginx',
-        'Make sure stop your local nginx with (service nginx stop)',
-      ],
+      docker_compose: ['Docker compose :whale: :whale:', '(https://docs.docker.com/compose/install/)'],
+      nginx: ['Ready for start nginx', 'Make sure stop your local nginx with (service nginx stop)'],
     };
     super({ ...data, messages: { ...defaultMessages, ...data.messages } });
   }
 
-  checkDocker({
-    docker: [minVersion, maxVersion],
-    dockerCompose: [composeMinVersion, composeMaxVersion],
-  }) {
+  checkDocker({ docker: [minVersion, maxVersion], dockerCompose: [composeMinVersion, composeMaxVersion] }) {
     print.log('******* :whale2: Check Docker versions :whale2: *******');
 
-    this.checkVersion(
-      'docker',
-      exec(`docker version --format '{{.Server.Version}}'`),
-      [minVersion, maxVersion],
-    );
-    this.checkVersion(
-      'docker_compose',
-      exec(`docker-compose version --short`),
-      [composeMinVersion, composeMaxVersion],
-    );
+    this.checkVersion('docker', exec(`docker version --format '{{.Server.Version}}'`), [minVersion, maxVersion]);
+    this.checkVersion('docker_compose', exec(`docker-compose version --short`), [composeMinVersion, composeMaxVersion]);
   }
 }
 
--- src/EnvironmentChecksBase.test.js
+++ src/EnvironmentChecksBase.test.js
@@ -6,35 +6,55 @@ describe('EnvironmentChecksBase', () => {
   test('#isSuccess', () => {
     expect(environmentChecks.isSuccess()).toEqual(true);
   });
+  test('#getMessages', () => {
+    environmentChecks = new EnvironmentChecksBase({
+      messages: {
+        docker: ['Custom Success', 'Custom error'],
+      },
+    });
+
+    expect(environmentChecks.getMessages('docker')).toEqual({
+      failure: 'Custom error',
+      success: 'Custom Success',
+    });
+  });
+
   describe('#checkVersion', () => {
+    let versionRage = ['10.1.0', '11.0.0'];
     test('when version is in the rage', () => {
-      const result = environmentChecks.checkVersion('docker', '10.1.1', [
-        '10.1.0',
-        '11.0.0',
-      ]);
+      const result = environmentChecks.checkVersion('docker', '10.1.1', versionRage);
       expect(result).toEqual(true);
     });
 
     test('when version is not in the rage', () => {
       const spy = jest.spyOn(print, 'error');
-      const result = environmentChecks.checkVersion('docker', '12.1.1', [
-        '10.1.0',
-        '11.0.0',
-      ]);
+      const result = environmentChecks.checkVersion('docker', '12.1.1', versionRage);
 
       expect(result).toEqual(false);
-      expect(spy).toHaveBeenCalledTimes(1);
+      expect(spy).toHaveBeenCalled();
       expect(environmentChecks.errors).toEqual(['docker']);
     });
 
     test('when version is not defined', () => {
       const spy = jest.spyOn(print, 'error');
-      const result = environmentChecks.checkVersion('docker', undefined, [
-        '10.1.0',
-        '11.0.0',
-      ]);
+      const result = environmentChecks.checkVersion('docker', undefined, versionRage);
       expect(result).toEqual(false);
-      expect(spy).toHaveBeenCalledTimes(1);
+      expect(spy).toHaveBeenCalled();
+    });
+
+    test('when version have custom message', () => {
+      let environmentChecks = new EnvironmentChecksBase({
+        messages: {
+          docker: ['Custom Success', 'Custom error'],
+        },
+      });
+
+      const spy = jest.spyOn(print, 'error');
+      const result = environmentChecks.checkVersion('docker', undefined, versionRage);
+      expect(result).toEqual(false);
+      expect(spy).toHaveBeenCalledWith(
+        `:mega: [Error] Not found version for docker; check your installation of docker Custom error`
+      );
       expect(environmentChecks.errors).toEqual(['docker']);
     });
   });
